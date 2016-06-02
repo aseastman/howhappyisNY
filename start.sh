af4fae38ac9bb407ecf6c95e72981b5dda88bac3
aseastman <aeastman@arrow.com>
2016-06-02 11:36:41 -0600

    re-pointed the path to all the requisite jar files

--- start.sh
+++ start.sh
@@ -8,8 +8,6 @@ env SPARK_HOME=/opt/mapr/spark/spark-1.6.1 /opt/mapr/spark/spark-1.6.1/bin/spark
 /home/user03/.ivy2/cache/com.ning/async-http-client/jars/async-http-client-1.9.21.jar,\
 /home/user03/.ivy2/cache/io.netty/netty/bundles/netty-3.10.1.Final.jar,\
 /home/user03/.ivy2/cache/org.jboss.netty/netty/bundles/netty-3.2.2.Final.jar,\
-/home/user03/twitter_demo/twitter_streaming_demo/lib/httpclient-4.5.2.jar,\
-/home/user03/twitter_demo/twitter_streaming_demo/lib/httpcore-4.4.4.jar,\
 /home/user03/.ivy2/cache/commons-codec/commons-codec/jars/commons-codec-1.10.jar,\
 /home/user03/.ivy2/cache/com.googlecode.efficient-java-matrix-library/ejml/jars/ejml-0.23.jar \
 --num-executors 1 --executor-cores 5 --driver-memory 3G --executor-memory 6G target/scala-2.10/spark-h_2.10-0.1.jar
