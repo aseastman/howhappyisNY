642a3f8e1ed76e6ce03bfbdbc808ad1eeeb33b9d
aseastman <aeastman@arrow.com>
2016-06-02 11:40:17 -0600

    re-pointed the path to all the requisite jar files

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 env SPARK_HOME=/opt/mapr/spark/spark-1.6.1 /opt/mapr/spark/spark-1.6.1/bin/spark-submit --master yarn-cluster \
---conf spark.akka.frameSize=200 --files /etc/hive/conf/hive-site.xml \
+--conf spark.akka.frameSize=200 \
 --jars /home/user03/.ivy2/cache/org.apache.spark/spark-streaming-twitter_2.10/jars/spark-streaming-twitter_2.10-1.5.0.jar,\
 /home/user03/.ivy2/cache/edu.stanford.nlp/stanford-corenlp/jars/stanford-corenlp-3.6.0.jar,\
 /home/user03/.ivy2/cache/edu.stanford.nlp/stanford-corenlp/jars/stanford-corenlp-3.6.0-models.jar,\
