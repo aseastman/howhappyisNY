fe1409ea3990d1c25ba444cb25f2f06b4b0d957c
aseastman <aeastman@arrow.com>
2016-06-02 10:53:03 -0600

    Doing an average of the sentiments over a 5 minute window.

--- src/main/scala/TwitterDriver.scala
+++ src/main/scala/TwitterDriver.scala
@@ -3,12 +3,13 @@ import org.apache.http.client.methods.{CloseableHttpResponse, HttpGet}
 import org.apache.http.impl.client.{DefaultHttpClient, HttpClientBuilder, HttpClients}
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.twitter._
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 import twitter4j.auth.{Authorization, AuthorizationFactory}
 import twitter4j.conf.ConfigurationBuilder
 import twitter4j.Status
 
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 
@@ -35,41 +36,53 @@ object TwitterDriver {
       .setOAuthAccessTokenSecret(accessTokenSecret)
 
     val auth : Authorization = AuthorizationFactory.getInstance(cb.build())
+    var sentimentValues : ArrayBuffer[Double] = ArrayBuffer.empty[Double]
+    var counter : Long = 0
 
 
-    val filters : Array[String] = Array("#fiveyearsout")
+    val filters : Array[String] = Array("#NY","#New York","#Manhattan")
 
     val tweets : DStream[Status] = TwitterUtils.createStream(ssc, Option(auth),filters)
 
 
-    tweets.foreachRDD{tweetRDD =>
+    tweets.window(Minutes(5),Seconds(30)).foreachRDD{ tweetRDD =>
       tweetRDD.foreach{tweet =>
           val lang = tweet.getUser.getLang
         if (lang == "en") {
           val username : String = tweet.getUser.getScreenName
           val friends : Long = tweet.getUser.getFriendsCount
-          val text : String = tweet.getText.split("https")(0).replaceAll("[^a-zA-Z0-9.!?'%@ /n]","").replaceAll("/n"," ")
+          val text : String = tweet.getText.split("https")(0).replaceAll("[^a-zA-Z0-9.!?'%@ ]","").replaceAll("/n"," ")
           val textCount : Long = text.split(" ").length
-          val sentimentValue : Double = SentimentAnalysis.detectSentiment(text)
-          val sentiment : String = if (sentimentValue <= 0.0) {
-            "Not Understood"
-          } else if (sentimentValue <= 1.0) {
-            "Very Negative"
-          } else if (sentimentValue <= 2.0) {
-            "Negative"
-          } else if (sentimentValue <= 3.0) {
-            "Neutral"
-          } else if (sentimentValue <= 4.0) {
-            "Positive"
-          } else if (sentimentValue <= 5.0) {
-            "Very Positive"
-          } else "Not Understood"
-          if (sentiment != "Not Understood") {
-            println(s"$username is $sentiment has tweeted '$text' ($textCount words) and has $friends friends.")
+          val sentiment : Double = SentimentAnalysis.detectSentiment(text)
+//          val sentiment : String = if (sentimentValue <= 0.0) {
+//            "Not Understood"
+//          } else if (sentimentValue <= 1.0) {
+//            "Very Negative"
+//          } else if (sentimentValue <= 2.0) {
+//            "Negative"
+//          } else if (sentimentValue <= 3.0) {
+//            "Neutral"
+//          } else if (sentimentValue <= 4.0) {
+//            "Positive"
+//          } else if (sentimentValue <= 5.0) {
+//            "Very Positive"
+//          } else "Not Understood"
+          if (sentiment > 0.0 && sentiment <= 5.0) {
+//            println(s"$username is $sentiment has tweeted '$text' ($textCount words) and has $friends friends.")
+            sentimentValues += sentiment
+            counter += 1
           }
         }
       }
-//      println(owu.getWeather("Denver"))
+      val avgSentiment = sentimentValues.sum/counter.toDouble
+
+      println(s"The average sentiment is $avgSentiment")
+
+
+      println(owu.getWeather("New York"))
+
+      sentimentValues = ArrayBuffer.empty[Double]
+      counter = 0
     }
     ssc.start()
     ssc.awaitTermination()
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-env HADOOP_CONF_DIR=/etc/hive/conf spark-submit --master yarn-cluster \
+env SPARK_HOME=/opt/mapr/spark/spark-1.6.1 /opt/mapr/spark/spark-1.6.1/bin/spark-submit --master yarn-cluster \
 --conf spark.akka.frameSize=200 --files /etc/hive/conf/hive-site.xml \
 --jars /home/a78084/.ivy2/cache/org.apache.spark/spark-streaming-twitter_2.10/jars/spark-streaming-twitter_2.10-1.5.0.jar,\
 /home/a78084/.ivy2/cache/edu.stanford.nlp/stanford-corenlp/jars/stanford-corenlp-3.6.0.jar,\
